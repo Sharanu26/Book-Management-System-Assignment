fdc1d038789abd6512b6b700f97845f257c3febb
sharanaabasppa <hallesharanu5@mail.com>
2026-05-26 22:44:52 +0530

    add cors, updated axios

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
-
+server.use(cors())
 server.use(middlewares);
 server.use(router);
 
--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://book-management-system-assignment-4.onrender.com",
 });
 
 export default api;
